a44aca380f4295ed33a4a265b1c7b4abe57c1f2b
Cameron Loren <camm.loren@yahoo.com>
2026-03-31 12:33:40 -0400

    moved goober-bot manifests to own subfolder

--- envs/prod/goober-bot/backend.tf
+++ envs/prod/goober-bot/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "goober-bot-terraform-state"
-    key            = "prod/terraform.tfstate"
+    key            = "prod/goober-bot/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-state-lock"
     encrypt        = true
